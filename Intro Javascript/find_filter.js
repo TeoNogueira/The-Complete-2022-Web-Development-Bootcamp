9aeaab3439056fbab29425f4486278c48e8a6206
Teo Nogueira <teonogueira17@gmail.com>
2022-04-27 02:31:46 -0300

    training

--- Intro Javascript/find_filter.js
+++ Intro Javascript/find_filter.js
@@ -3,13 +3,13 @@ const arr = [15, 42, -15, 75, 250, 200, 300, 11]
 
 const testFind = arr.find(logicalFindSocket)
 
-console.log(testFind) // get in order 
+// console.log(testFind) // get in order 
 
 
 function logicalFindSocket(fMore) {
 
 
-    return fMore > 75
+    return fMore > 75 
     
 }
 
@@ -17,18 +17,22 @@ function logicalFindSocket(fMore) {
 // FILTER
 
 
-const arr2 = [15, 42, -15, 75, 250, 200, 300, 11]
+let arr2 = [15, 42, -15, 75, 250, 200, 300, 11]
+
+
 
 
 const testFind2 = arr2.filter(logicalFindSocket)
 
+
+
 console.log(testFind2) // get in order 
 
 
-function logicalFindSocket(fMore) {
+function logicalFindSocket(fMor) {
 
 
-    return fMore > 75
+    return fMor >= 75
     
 }
 
--- Intro Javascript/tempCodeRunnerFile.js
+++ Intro Javascript/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-const testFind = arr.find(logicalFindSocket)
+testFind3
