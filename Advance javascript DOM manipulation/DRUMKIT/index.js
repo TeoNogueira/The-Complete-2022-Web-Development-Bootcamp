499886b4a173457cb98688329a5d268fcca8e223
Teo Nogueira <teonogueira17@gmail.com>
2022-05-02 16:44:35 -0300

    Finished

--- Advance javascript DOM manipulation/DRUMKIT/index.js
+++ Advance javascript DOM manipulation/DRUMKIT/index.js
@@ -12,16 +12,76 @@ const interaClass = document.querySelectorAll('.drum').length
 for(var i = 0; i < interaClass; i++) {
 
 const getButton = document.querySelectorAll('button');
+/*
+let audio = new Audio('./sounds/tom-3.mp3')
 
-getButton[i].addEventListener('click', function(e) {
-
-// let audio = new Audio('./sounds/tom-3.mp3')
-
-// audio.play()
+ audio.play()
 
 this.style.color = '#bbddaa';
 
+*/
+
+getButton[i].addEventListener('click', function(e) {
+
+let buttonInnerHTML = this.innerHTML;
+
+getSound(buttonInnerHTML)
 
 })
 
 }
+
+// PUSH SWITCHS FOR KEYSPRESS
+document.addEventListener('keypress', (e) => {
+
+
+getSound(e.key)
+
+})
+
+
+
+        function getSound(key) {
+                
+switch(key) {
+
+        case "w":
+         let audio = new Audio('./sounds/tom-1.mp3')
+         audio.play() 
+        break;
+        
+        case "a":
+        let audio2 = new Audio('./sounds/tom-2.mp3')
+        audio2.play() 
+        break;
+        case "s":
+        let audio3 = new Audio('./sounds/tom-3.mp3')
+        audio3.play() 
+        break;
+        
+        case "d":
+        let audio4 = new Audio('./sounds/tom-4.mp3')
+        audio4.play() 
+        break;
+        
+        case "j":
+        let snare = new Audio('./sounds/snare.mp3')
+        snare.play() 
+        break;
+        
+        case "k":
+        let crash = new Audio('./sounds/crash.mp3')
+        crash.play() 
+        break;
+        
+        case "l":
+        let kickBass = new Audio('./sounds/kick-bass.mp3')
+        kickBass.play() 
+        break;
+        default: 
+        // default: console.log(buttonInnerHMTL)
+        
+        
+        
+        }
+        }
---
+++ Advance javascript DOM manipulation/param.js
@@ -0,0 +1,11 @@
+function dPersons(nome, age, clan, gold) {
+
+        this.nome = nome
+        this.age = age
+        this.clan = clan
+        this.gold = gold 
+}
+
+const recipe = new dPersons('Robot', 25, true, 5000000)
+
+
